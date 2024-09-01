0d45e689d83d966bf8ee9de91c7dc13ecf70817f
maxnes3 <draw_max@mail.ru>
2024-09-01 17:42:51 +0400

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,27 @@
+import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslint from 'typescript-eslint';
 import prettierPlugin from 'eslint-plugin-prettier';
-import tsParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default [
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    ignores: ['dist', 'node_modules'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: tsParser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      '@typescript-eslint': tseslint,
       prettier: prettierPlugin,
     },
     rules: {
@@ -39,4 +43,4 @@ export default [
       ],
     },
   },
-];
+);
